786cb15e81a4298e002aca9874a92264a184767a
Emil Petersson <emilpet93@gmail.com>
2020-03-31 17:23:59 +0200

    Project card maps props info

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,34 @@
 import React from "react"
 import styled from "styled-components"
+import { format } from "date-fns"
 
-const CardContainer = styled.div`
-  border: 1px thin #555;
+const CardContainer = styled.li`
   background: #fff;
+  border: 1px thin #555;
+  border-radius: 4px;
+  box-shadow: 2px 2px 5px #787878;
   padding: 1rem;
   min-width: 1000px;
 `
 
-const ProjectCard = ({ url, title, description, language, test }) => {
-  return (
-    <CardContainer>
-      <a href={url} style={{ textDecoration: "none" }}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <span>{language}</span>
-      </a>
-    </CardContainer>
-  )
+const ProjectCard = ({ cardInfo }) => {
+  return cardInfo.map(info => {
+    const { id, html_url, name, description, updated_at, language } = info
+    return (
+      <CardContainer key={id}>
+        <a
+          title={`View ${name} on Github`}
+          href={html_url}
+          style={{ textDecoration: "none" }}
+        >
+          <h3>{name}</h3>
+          <p>{description}</p>
+          <p>Last updated {format(new Date(updated_at), "dd MMMM yyyy")}</p>
+          <span>{language}</span>
+        </a>
+      </CardContainer>
+    )
+  })
 }
 
 export default ProjectCard
