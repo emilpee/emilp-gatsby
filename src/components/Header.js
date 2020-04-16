2459b0fcce73b5a3e0c3fd475459ede4b1f35234
Emil Petersson <emilpet93@gmail.com>
2020-04-16 18:18:23 +0200

    Links data added in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,57 @@ import Menu from './Menu'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rgba(0, 0, 0, .8)',
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const links = [
+    {
+      name: 'Home',
+      link: '/',
+      isActive: false,
+    },
+    {
+      name: 'About',
+      link: '/about',
+      isActive: false,
+    },
+    {
+      name: 'Contact',
+      link: '/contact',
+      isActive: false,
+    },
+  ]
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        background: 'rgba(0, 0, 0, .8)',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Menu />
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <Menu items={links} />
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
