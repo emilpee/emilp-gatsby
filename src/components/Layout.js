e73087cc277c092d9706339836ee888992f65819
Emil Petersson <emilpet93@gmail.com>
2020-04-16 18:19:01 +0200

    Menu links has a nice transition

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Header from './Header'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { GlobalStyle } from '../styles/StyledComponents'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab, faLinkedin, faGithub, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fab, faGithub, faLinkedin, faFacebookSquare)
@@ -33,20 +34,36 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          background: 'linear-gradient(to right, #fff, #ddd)',
+          boxShadow: '2px 1px 3px rgba(0, 0, 0, 0.4)',
+          padding: '.75rem',
         }}
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()} {data.site.siteMetadata.title}
+          <div>
+            <FontAwesomeIcon
+              icon={['fab', 'github']}
+              style={{ color: '#222', fontSize: '1.8em' }}
+            />
+            <FontAwesomeIcon
+              icon={['fab', 'linkedin']}
+              style={{ color: '#222', fontSize: '1.8em' }}
+            />
+            <FontAwesomeIcon
+              icon={['fab', 'facebook']}
+              style={{ color: '#222', fontSize: '1.8em' }}
+            />
+          </div>
         </footer>
       </div>
       <GlobalStyle />
     </>
   )
 }
+
+// TODO: lägg till props/style för fontawesome ikoner.
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
-const Menu = () => (
-  <MenuList>
-    <MenuItem>
-      <Link style={{ textDecoration: 'none', color: '#fff' }} to="/">
-        Home
-      </Link>
-    </MenuItem>
-    <MenuItem>
-      <Link style={{ textDecoration: 'none', color: '#fff' }} to="/about">
-        About
-      </Link>
-    </MenuItem>
-    <MenuItem>
-      <Link style={{ textDecoration: 'none', color: '#fff' }} to="/contact">
-        Contact
-      </Link>
-    </MenuItem>
-  </MenuList>
-)
+const Menu = ({ items }) => {
+  return (
+    <MenuList>
+      {items.map((item, index) => {
+        return (
+          <MenuItem key={index}>
+            <MenuLink
+              onClick={!items[index].isActive ? true : false}
+              style={{ fontWeight: `${items[index].isActive ? 700 : 400}` }}
+              to={item.link}
+            >
+              {item.name}
+            </MenuLink>
+          </MenuItem>
+        )
+      })}
+    </MenuList>
+  )
+}
 
 const MenuList = styled.ul`
   list-style: none;
@@ -36,6 +36,35 @@ const MenuList = styled.ul`
   }
 `
 
-const MenuItem = styled.li``
+const MenuItem = styled.li`
+  color: #fff;
+`
+
+const MenuLink = styled(Link)`
+  position: relative;
+  color: #fff;
+  text-decoration: none;
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    bottom: -3px;
+    left: 0;
+    background: #fff;
+    visibility: hidden;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transition: all 0.4s ease;
+    transition: all 0.4s ease;
+  }
+
+  &:hover:before {
+    visibility: visible;
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
+  }
+`
 
 export default Menu
