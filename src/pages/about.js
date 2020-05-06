cbdf8f080eb20865a938deb20b63b267e7d75036
Emil Petersson <emilpet93@gmail.com>
2020-05-06 17:52:48 +0200

    Fix render problem in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,21 +5,23 @@ import { Container } from '../styles/StyledComponents'
 import Image from '../components/Image'
 
 const AboutPage = () => {
-  ;<Layout>
-    <Container>
-      <SEO title="about" />
-      <h2>About me</h2>
-      <Image />
-      <p>
-        Löksås ipsum hav gör dag själv precis från bäckasiner hav samma, och mot mjuka att se helt
-        omfångsrik gamla och ingalunda, sista sax sällan tre som dock ingalunda kunde sällan. Stora
-        jäst dock jäst där och på sjö rot, tid bäckasiner annat ta inom tre år för, färdväg sin stig
-        kom fram se tiden. Vi så vidsträckt upprätthållande har ta strand sitt nu både ännu, hwila
-        varit smultron sällan tre tid färdväg vidsträckt därmed äng samma, dock strand det se
-        trevnadens göras färdväg genom trevnadens.
-      </p>
-    </Container>
-  </Layout>
+  return (
+    <Layout>
+      <Container>
+        <SEO title="about" />
+        <h2>About me</h2>
+        <Image />
+        <p>
+          Löksås ipsum hav gör dag själv precis från bäckasiner hav samma, och mot mjuka att se helt
+          omfångsrik gamla och ingalunda, sista sax sällan tre som dock ingalunda kunde sällan.
+          Stora jäst dock jäst där och på sjö rot, tid bäckasiner annat ta inom tre år för, färdväg
+          sin stig kom fram se tiden. Vi så vidsträckt upprätthållande har ta strand sitt nu både
+          ännu, hwila varit smultron sällan tre tid färdväg vidsträckt därmed äng samma, dock strand
+          det se trevnadens göras färdväg genom trevnadens.
+        </p>
+      </Container>
+    </Layout>
+  )
 }
 
 export default AboutPage
